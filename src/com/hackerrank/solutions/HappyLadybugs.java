65c2679424e36ace90454684c5e0f5a1b04768d8
Hardik khanna <khannahardik007@gmail.com>
2020-08-09 18:41:00 +0530

    Happy Ladybugs Problem added

---
+++ src/com/hackerrank/solutions/HappyLadybugs.java
@@ -0,0 +1,59 @@
+package com.hackerrank.solutions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class HappyLadybugs {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		while (n-- > 0) {
+			int size = sc.nextInt();
+			String input = sc.next();
+			happyLadybugs(input);
+		}
+		sc.close();
+	}
+
+	private static void happyLadybugs(String input) {
+		if ( !input.contains("_")) {
+			System.out.println("NO");
+			return;
+		}
+		if(input.length() == 1) {
+			System.out.println("YES");
+			return;
+		}
+
+		char[] arr = input.toCharArray();
+		Arrays.sort(arr);
+		input = "";
+		for (int i = 0; i < arr.length; i++) {
+			input += arr[i];
+		}
+
+		for (int i = 0; i < input.length(); i++) {
+			int count = 0;
+			for (int j = i + 1; j < input.length(); j++) {
+				if (input.charAt(i) != input.charAt(j)) {
+					break;
+				}
+				count++;
+			}
+			if (count > 0) {
+				String str = input.substring(i, i + count + 1);
+				// str = str.replace(str, "_");
+				input = input.replace(str, "_");
+			}
+		}
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) != '_') {
+				System.out.println("NO");
+				return;
+			}
+		}
+		System.out.println("YES");
+	}
+}
