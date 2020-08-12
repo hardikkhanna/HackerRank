5e04fd01eec79c306fdff65fa0b5076404b868fd
Hardik khanna <khannahardik007@gmail.com>
2020-08-12 11:21:38 +0530

    Mars Exploration Problem

--- src/com/hackerrank/solutions/MarsExploration.java
+++ src/com/hackerrank/solutions/MarsExploration.java
@@ -1,10 +1,21 @@
 package com.hackerrank.solutions;
 
+import java.util.Scanner;
+
 public class MarsExploration {
 
 	public static void main(String[] args) {
 
-		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int count = 0;
+		String sos = "SOS";
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != sos.charAt(i % 3))
+				count++;
+		}
+		System.out.println(count);
+		sc.close();
 
 	}
 
