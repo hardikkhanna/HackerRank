d59b62445a51e0948c496c13ffff7daea948258f
Hardik khanna <khannahardik007@gmail.com>
2020-08-12 11:55:14 +0530

    Added Pangrams problem

---
+++ src/com/hackerrank/solutions/Pangrams.java
@@ -0,0 +1,30 @@
+package com.hackerrank.solutions;
+
+import java.util.Scanner;
+
+public class Pangrams {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		int[] freq = new int[26];
+		str = str.toLowerCase();
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == ' ') {
+				continue;
+			}
+			freq[str.charAt(i) - 'a']++;
+		}
+		for (int i = 0; i < freq.length; i++) {
+			if (freq[i] == 0) {
+				System.out.println("not pangram");
+				return;
+			}
+		}
+		System.out.println("pangram");
+		sc.close();
+
+	}
+
+}
