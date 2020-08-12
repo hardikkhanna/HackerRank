bc87899e49d11cd6e72c7e699bc8fd6f17e027c8
Hardik khanna <khannahardik007@gmail.com>
2020-08-12 22:14:13 +0530

    Added LoveLetter Mystery Problem

---
+++ src/com/hackerrank/solutions/LoveLetterMystery.java
@@ -0,0 +1,24 @@
+package com.hackerrank.solutions;
+
+import java.util.Scanner;
+
+public class LoveLetterMystery {
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    for(int ii = 0; ii < n; ii++){
+	        String s = sc.next();
+	        int count = 0;
+	        // use two pointers;
+	        int i = 0, j = s.length() - 1;
+	        while(i < j){
+	            count += Math.abs(s.charAt(i) - s.charAt(j));
+	            i++; j--;
+	        }
+	        System.out.println(count);
+	    }
+	    sc.close();
+	}
+
+}
