96cb115dd5c9f9e423ff9e54d8dda8bbfbc3de91
Hardik khanna <khannahardik007@gmail.com>
2020-08-18 16:33:05 +0530

    Added lonely integer

---
+++ src/com/hackerrank/solutions/LonelyInteger.java
@@ -0,0 +1,28 @@
+package com.hackerrank.solutions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LonelyInteger {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int ans = lonelyInteger(arr);
+		System.out.println(ans);
+		sc.close();
+	}
+
+	private static int lonelyInteger(int[] arr) {
+		int val = 0;
+		for (int num : arr) {
+			val = val ^ num; // ^ is XOR operator
+		}
+		return val;
+	}
+
+}
